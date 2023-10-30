53bd36cb089eaa62d51d536baa6d72c5839fff26
walysonrodrigo <walysonrodrigo@gmail.com>
2023-10-30 19:09:36 -0300

    add: rule uniqueapplujob

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,7 +2,15 @@
 
 namespace App\Providers;
 
+
+use App\Http\Interfaces\CandidateJobsRepositoryInterface;
+use App\Http\Interfaces\CandidatesRepositoryInterface;
+use App\Http\Interfaces\JobsRepositoryInterface;
+use App\Http\Repositories\CandidateJobsRepository;
+use App\Http\Repositories\CandidatesRepository;
+use App\Http\Repositories\JobsRepository;
 use Illuminate\Support\ServiceProvider;
+
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -11,7 +19,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        //
+        $this->app->bind(CandidatesRepositoryInterface::class, CandidatesRepository::class);
+        $this->app->bind(JobsRepositoryInterface::class, JobsRepository::class);
+        $this->app->bind(CandidateJobsRepositoryInterface::class, CandidateJobsRepository::class);
     }
 
     /**
