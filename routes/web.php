d91b5cb2ba8b0edfca29a05179f8246f85259fc2
walysonrodrigo <walysonrodrigo@gmail.com>
2023-10-31 21:14:21 -0300

    feat: add routes web

--- routes/web.php
+++ routes/web.php
@@ -15,4 +15,32 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
-});
+})->name('home');
+
+Route::get('/jobs-index', function () {
+    return view('pages.jobs.index');
+})->name('jobs.index');
+
+Route::get('/cadastrar-vaga', function () {
+    return view('pages.jobs.form');
+})->name('jobs.form');
+
+Route::get('/editar-vaga/{id}', function () {
+    return view('pages.jobs.form');
+})->name('jobsedit.form');
+
+Route::get('/candidates-index', function () {
+    return view('pages.candidates.index');
+})->name('candidates.index');
+
+Route::get('/cadastrar-profissional', function () {
+    return view('pages.candidates.form');
+})->name('candidates.form');
+
+Route::get('/applications', function () {
+    return view('pages.candidates.applications');
+})->name('candidates.applications');
+
+Route::get('/apply', function () {
+    return view('pages.candidates.apply');
+})->name('candidates.apply');
