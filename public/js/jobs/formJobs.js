77aeefc35b2a4ac1e2c92cad9d70ef44e5e3d49c
walysonrodrigo <walysonrodrigo@gmail.com>
2023-10-31 21:11:37 -0300

    feat: add formJobs js

---
+++ public/js/jobs/formJobs.js
@@ -0,0 +1,28 @@
+$(document).ready(function () {
+  $('#addJobs').on('click', function (event) {
+    event.preventDefault(); // Evita o comportamento padrão de envio do formulário
+    var title = $('#title').val();
+    var type = $('#type').val();
+    var active = $('input[name="flexRadioDefault"]:checked').val();
+
+    var data = {
+      title,
+      type,
+      active
+    };
+
+    $.ajax({
+      url: 'http://localhost:3000/api/jobs/',
+      type: 'POST',
+      contentType: 'application/json',
+      data: JSON.stringify(data),
+      success: function (response) {
+        alert("Vaga cadastrada com sucesso!");
+      },
+      error: function (error) {
+        console.error('Erro:', error);
+        alert("Erro ao cadastrar vaga! Verifique os campos e tente novamente.");
+      }
+    });
+  });
+});
