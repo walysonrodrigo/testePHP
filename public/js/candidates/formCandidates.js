934ce81d85e0f3fc7737d43cf71b6eb6815c9a69
walysonrodrigo <walysonrodrigo@gmail.com>
2023-10-31 21:11:25 -0300

    feat: add formCandidates js

---
+++ public/js/candidates/formCandidates.js
@@ -0,0 +1,27 @@
+$(document).ready(function () {
+  $('#addCandidates').on('click', function (event) {
+    event.preventDefault(); // Evita o comportamento padrão de envio do formulário
+    var name = $('#name').val();
+    var email = $('#email').val();
+
+    // Monta o objeto de dados
+    var data = {
+      name,
+      email
+    };
+
+    $.ajax({
+      url: 'http://localhost:3000/api/candidates/',
+      type: 'POST',
+      contentType: 'application/json',
+      data: JSON.stringify(data),
+      success: function (response) {
+        alert("Candidato realizado com sucesso!");
+      },
+      error: function (error) {
+        console.error('Erro:', error);
+        alert("Erro ao cadastrar candidato! Verifique os campos e tente novamente.");
+      }
+    });
+  });
+});
